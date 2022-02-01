b463eca355ec0983d0b94a8955ab0ccefb1e3b94
daniel <dacruva@gmail.com>
2022-02-01 15:28:26 -0500

    Added Logger

--- src/main/java/com/course/practicaljava/api/server/DefaultRestApi.java
+++ src/main/java/com/course/practicaljava/api/server/DefaultRestApi.java
@@ -1,6 +1,8 @@
 package com.course.practicaljava.api.server;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,12 @@ import java.time.LocalTime;
 @RestController
 @RequestMapping(value = "/api")
 public class DefaultRestApi {
+
+    private Logger LOG = LoggerFactory.getLogger(DefaultRestApi.class);
+
     @GetMapping(value = "/welcome")
     public String welcome() {
-        System.out.println(StringUtils.join("Hello ", "this is ", "spring boot ", "rest API"));
+        LOG.info(StringUtils.join("Hello ", "this is ", "spring boot ", "REST API"));
         return "Welcome to spring boot";
     }
     @GetMapping(value = "/time")
